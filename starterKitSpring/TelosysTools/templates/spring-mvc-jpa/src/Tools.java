734ab168ac8acb565a3a65e1c3ae4e9b992ebd68
ludo1026 <l.chaboud@gmail.com>
2014-01-11 01:04:20 +0100

    ordre

--- starterKitSpring/TelosysTools/templates/spring-mvc-jpa/src/Tools.java
+++ starterKitSpring/TelosysTools/templates/spring-mvc-jpa/src/Tools.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.telosys.tools.db.model.DatabaseColumn;
+import org.telosys.tools.db.model.DatabaseTable;
+import org.telosys.tools.db.model.DatabaseTables;
 import org.telosys.tools.generator.context.JavaBeanClass;
 import org.telosys.tools.generator.context.JavaBeanClassAttribute;
 import org.telosys.tools.generator.context.JavaBeanClassForeignKey;
@@ -12,6 +16,218 @@ import org.telosys.tools.generator.context.JavaBeanClassLink;
 import org.telosys.tools.generator.context.Model;
 
 public class Tools {
+	
+	public String toto(DatabaseTables databaseTables, Model model, List<JavaBeanClass> entities) {
+		String str = "";
+		
+//		for(JavaBeanClass entity : entities) {
+//			str += "\n"+entity.getName();
+//			for(JavaBeanClassLink link : entity.getLinks()) {
+//				str += "("+link.getJavaName()+","+link.getOptional()+")";
+//			}
+//		}
+//		if(true) {
+//			return str;
+//		}
+		
+		Entitys entitys = new Entitys();
+		for(JavaBeanClass entity : entities) {
+			str += entity.getName() + ",";
+			entitys.get(entity.getName());
+		}
+		
+
+		str += "\n";
+		
+		Entity entity = entitys.getNextNotDone();
+		int count = 0;
+		while(entity != null && count < 30) {
+			str += entity.name+",";
+			explore(databaseTables, model, entity.name, entitys, 0);
+			entity = entitys.getNextNotDone();
+			count++;
+		}
+		
+		str += "\n";
+		
+		for(Entity entity2 : entitys.entitys) {
+			str += entity2.name+",";
+			// export(entity2.name);
+		}
+		
+		return str;
+	}
+	
+	public void explore(DatabaseTables databaseTables, Model model, String current, Entitys entitys, int depth) {
+		
+		if(depth==15) {
+			return;
+		}
+		
+		Entity entity = entitys.get(current);
+		if(entity.done) {
+			return;
+		}
+		entity.done = true;
+
+		boolean containsEntity = false;
+		for(Entity entity2 : entitys.entitys) {
+			if(entity.name.equals(entity2.name)) {
+				containsEntity = true;
+			}
+		}
+		if(!containsEntity) {
+			entitys.entitys.add(entity);
+		}
+		
+		List<String> entityNameBefores = befores(databaseTables, model.getEntityByClassName(current));
+		for(String entityName : entityNameBefores) {
+			entitys.setBefore(current, entityName);
+			explore(databaseTables, model, entityName, entitys, depth+1);
+		}
+
+		List<String> entityNameAfters = afters(databaseTables, model.getEntityByClassName(current));
+		for(String entityName : entityNameAfters) {
+			entitys.setAfter(current, entityName);
+			explore(databaseTables, model, entityName, entitys, depth+1);
+		}
+		
+	}
+
+	public static class Entitys {
+		public Map<String,Entity> entityByNames = new HashMap<String,Entity>();
+		public Entity get(String name) {
+			Entity entity = entityByNames.get(name);
+			if(entity == null) {
+				entity = new Entity();
+				entity.name = name;
+				entityByNames.put(name, entity);
+			}
+			return entity;
+		}
+		public Entity getNextNotDone() {
+			Entity entityNext = null;
+			for(Entity entity : entityByNames.values()) {
+				if(!entity.done) {
+					entityNext = entity;
+				}
+			}
+			return entityNext;
+		}
+		public LinkedList<Entity> entitys = new LinkedList<Entity>();
+		public void setBefore(String entityRoot, String entityBefore) {
+			setBefore(get(entityRoot), get(entityBefore));
+		}
+		public void setAfter(String entityRoot, String entityBefore) {
+			setAfter(get(entityRoot), get(entityBefore));
+		}
+		public void setBefore(Entity entityRoot, Entity entityBefore) {
+			entityRoot.entitysAvant.add(entityBefore);
+			int index = -1;
+			for(Entity entity : entitys) {
+				if(entity.name.equals(entityBefore.name)) {
+					return;
+				}
+			}
+			for(Entity entity : entitys) {
+				index++;
+				if(entity.name.equals(entityRoot.name)) {
+					break;
+				}
+			}
+			if(index == -1) {
+				// liste vide : on définit le entityRoot
+				entitys.add(entityBefore);
+				entitys.add(entityRoot);
+			} else {
+				entitys.add(index, entityBefore);
+			}
+		}
+		public void setAfter(Entity entityRoot, Entity entityAfter) {
+			entityRoot.entitysAvant.add(entityAfter);
+			for(Entity entity : entitys) {
+				if(entity.name.equals(entityAfter.name)) {
+					return;
+				}
+			}
+			int index = -1;
+			for(Entity entity : entitys) {
+				index++;
+				if(entity.name.equals(entityRoot.name)) {
+					break;
+				}
+			}
+			if(index == -1) {
+				// liste vide : on définit le entityRoot
+				entitys.add(entityRoot);
+				entitys.add(entityAfter);
+			} else {
+				entitys.add(index+1, entityAfter);
+			}
+		}
+	}
+	
+	public static class Entity {
+		public String name;
+		public List<Entity> entitysAvant = new ArrayList<Tools.Entity>();
+		public List<Entity> entitysApres = new ArrayList<Tools.Entity>();
+		public boolean done = false;
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+	
+	private List<String> befores(DatabaseTables databaseTables, JavaBeanClass entity) {
+		List<String> befores = new ArrayList<String>();
+		for(JavaBeanClassLink link : entity.getLinks()) {
+			if(isBefore(databaseTables, link)) {
+				befores.add(link.getTargetEntityType());
+			}
+		}
+		return befores;
+	}
+	
+	private List<String> afters(DatabaseTables databaseTables, JavaBeanClass entity) {
+		List<String> afters = new ArrayList<String>();
+		for(JavaBeanClassLink link : entity.getLinks()) {
+			if(!isBefore(databaseTables, link)) {
+				afters.add(link.getTargetEntityType());
+			}
+		}
+		return afters;
+	}
+	
+	private boolean isBefore(DatabaseTables databaseTables, JavaBeanClassLink link) {
+//		boolean isBefore = link.getCardinality().equals("ManyToOne");
+		boolean isBefore = false;
+		DatabaseTable databaseTable = databaseTables.getTableByName(link.getJoinTable());
+		for(String columnName : link.getJoinColumns()) {
+			DatabaseColumn databaseColumn = databaseTable.getColumnByName(columnName);
+			if(databaseColumn.isNotNull()) {
+				isBefore = true;
+			}
+		}
+		return isBefore;
+	}
+	
+	// ------------------------------------------------------------------------
+	
+	
+	public boolean isText(JavaBeanClassAttribute attribute) {
+		String[] types = {"String"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
+	
+	public boolean isNumber(JavaBeanClassAttribute attribute) {
+		String[] types = {"Integer","Long","Double","Short","Byte","Float","Number","BigInteger","BigDecimal"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
+
+	public boolean isDate(JavaBeanClassAttribute attribute) {
+		String[] types = {"Date","Calendar"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
 	
 	/**
 	 * Retourne l'attribut de la clé.
@@ -284,5 +500,23 @@ public class Tools {
 		}
 		return fieldsMappingForLink;
 	}
-
+	
+	public List<JavaBeanClassLink> links(JavaBeanClass entity, int... criterias) {
+		if(criterias == null || criterias.length == 0) {
+			return entity.getLinks();
+		}
+		List<JavaBeanClassAttribute> attributes = new ArrayList<JavaBeanClassAttribute>();
+		if(criterias.length == 1) {
+			attributes = entity.getAttributesByCriteria(criterias[0]);
+		} else if(criterias.length == 2) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1]);
+		} else if(criterias.length == 3) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1], criterias[2]);
+		} else if(criterias.length == 4) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1], criterias[2], criterias[3]);
+		} 
+		// Récupérer les liens à partir des attributs
+		return linksForFields(entity.getLinks(), attributes);
+	}
+	
 }
